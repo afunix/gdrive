9f3f9c76d44386a0e288223a94db3f9a33a91df5
Pavel Malyshev <p.malishev@gmail.com>
2021-03-05 12:09:49 -0600

    Changed github.com/prasmussen to github.com/afunix

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/afunix/gdrive
 go 1.15
 
 require (
-	github.com/prasmussen/gdrive v0.0.0-20210305150821-b854c97ae2da
 	github.com/sabhiram/go-gitignore v0.0.0-20201211210132-54b8a0bf510f
 	github.com/soniakeys/graph v0.0.0-20160409104831-c265d9676750
 	golang.org/x/net v0.0.0-20200822124328-c89045814202
--- handlers_drive.go
+++ handlers_drive.go
@@ -9,9 +9,9 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/prasmussen/gdrive/auth"
-	"github.com/prasmussen/gdrive/cli"
-	"github.com/prasmussen/gdrive/drive"
+	"github.com/afunix/gdrive/auth"
+	"github.com/afunix/gdrive/cli"
+	"github.com/afunix/gdrive/drive"
 )
 
 const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
